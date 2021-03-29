913406b073218617ba3a6263bbe36bb18d3e6737
Blackscure <wekesabuyahi@gmail.com>
2021-03-29 15:08:40 +0300

    Urlshortner files

---
+++ src/main/java/com/yahi/Q1urlshorter/Q1urlshorterApplication.java
@@ -0,0 +1,13 @@
+package com.yahi.Q1urlshorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q1urlshorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q1urlshorterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yahi/Q1urlshorter/Q1urlshorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahi.Q1urlshorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q1urlshorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
